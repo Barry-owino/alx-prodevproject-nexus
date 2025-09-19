f58c86e80b7f263929793766b1625dfbadb172f7
Barry-owino <barryowino5@gmail.com>
2025-09-19 23:28:24 +0300

    created views for user login and register

--- ecommerce_backend/users/views.py
+++ ecommerce_backend/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from .serializers import UserRegisterSerializer, UserLoginSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+# Register View
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+
+
+# Login View
+class LoginView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        email = serializer.validated_data["email"]
+        password = serializer.validated_data["password"]
+        user = authenticate(request, email=email, password=password)
+
+        if not user:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key, "user": {"email": user.email, "username": user.username}})
+
